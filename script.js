20e1c1dc393be6e8ddf5378c37d8a71f12480dd0
Eggxellent <Peter.ngigi@strathmore.edu>
2025-07-02 21:54:27 +0300

    Security page

---
+++ script.js
@@ -0,0 +1,15 @@
+const scanBtn = document.getElementById('scanBtn');
+const otpBtn = document.getElementById('otpBtn');
+const otpField = document.getElementById('otpField');
+
+scanBtn.addEventListener('click', () => {
+  otpField.style.display = 'none';
+  scanBtn.classList.add('active');
+  otpBtn.classList.remove('active');
+});
+
+otpBtn.addEventListener('click', () => {
+  otpField.style.display = 'block';
+  otpBtn.classList.add('active');
+  scanBtn.classList.remove('active');
+});
